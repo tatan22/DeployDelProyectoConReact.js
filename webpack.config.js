2c61ec4fc03e1a7441f4440632371594a814d06c
Jhonatan-PC-Black <tatancarduar@gmail.com>
2023-03-24 09:08:17 -0500

    Modificaciones de errores

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const SccMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const {cleanWebpackPlugin, CleanWebpackPlugin} = require( 'clean-webpack-plugin');
 
