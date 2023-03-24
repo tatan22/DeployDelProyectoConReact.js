099e1a49d9f028697c288693c3cf5cfa5f4d7203
Jhonatan-PC-Black <tatancarduar@gmail.com>
2023-03-24 07:46:08 -0500

    Primer commit para subir Deploy netlify

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const SccMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const {cleanWebpackPlugin, CleanWebpackPlugin} = require( 'clean-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: "/"
+    },
+    resolve:{
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@Component': path.resolve(__dirname, 'src/components/'),
+            '@styles': path.resolve(__dirname,'src/style/')
+        }
+    },
+    module:{
+        rules: [
+            {
+               test: /\.(js|jsx)$/,
+               exclude: /node_modules/,
+               use: {
+                loader: 'babel-loader',
+               } 
+            },
+            {
+              test: /\.html$/,
+              use: [
+                {loader: 'html-loader'}
+              ]
+            },
+            {
+              test: /\.s[ac]ss$/, 
+              use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+              ] 
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename:'[name].css'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
+}
